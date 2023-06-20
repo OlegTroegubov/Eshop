7930afefd222e9dcdf4f4d9fcf81cb3965685332
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-06-21 04:48:10 +0700

    Added ClientOrderConfiguration

---
+++ Eshop/Persistence/Configuration/ClientOrderConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Eshop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eshop.Persistence.Configuration;
+
+public class ClientOrderConfiguration : IEntityTypeConfiguration<ClientOrder>
+{
+    public void Configure(EntityTypeBuilder<ClientOrder> builder)
+    {
+        builder
+            .HasKey(co => co.Id);
+        builder
+            .HasOne(co => co.Client)
+            .WithOne()
+            .HasForeignKey<ClientOrder>(co => co.ClientId);
+        builder
+            .HasOne(co => co.Order)
+            .WithOne()
+            .HasForeignKey<ClientOrder>(co => co.OrderId);
+    }
+}
