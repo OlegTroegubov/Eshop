a568c4d9ba6df34ba1fd6919b3bf800bee855dd3
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-06-20 21:44:02 +0700

    Created ClientController

---
+++ Eshop/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers;
+
+public class ClientController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
