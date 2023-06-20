145b28eea64fecbe35c5fcafa6ac7be916a79853
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-06-21 03:41:26 +0700

    Added Configuration for Product

---
+++ Eshop/Persistence/Configuration/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Eshop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Eshop.Persistence.Configuration;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder
+            .HasKey(p => p.Id);
+        builder
+            .Property(p => p.Name)
+            .HasMaxLength(250)
+            .IsRequired();
+        builder
+            .Property(p => p.Price)
+            .IsRequired();
+    }
+}
