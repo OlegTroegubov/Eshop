8509bcbce5a0654d5e49be1a05cb097e4fa136ff
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-06-25 09:10:58 +0700

    Edited ProductController

--- Eshop/Controllers/ProductController.cs
+++ Eshop/Controllers/ProductController.cs
@@ -1,12 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Eshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
-namespace Eshop.Controllers;
-
-public class ProductController : Controller
+namespace Eshop.Controllers
 {
-    // GET
-    public IActionResult Index()
+    public class ProductController : Controller
     {
-        return View();
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Product
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+
+        // GET: Product/Edit/5
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Product/Delete/5
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
