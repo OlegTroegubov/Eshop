d059fbf5bd3951f2ed21bc93b7b446e3d07a0f75
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-06-29 19:13:47 +0700

    New Migration

---
+++ Eshop/Migrations/20230629111733_FixedConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
