7f212108f6da12e609b8a5ec72d81ca6a9ff77e3
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-06-29 19:15:19 +0700

    Added post-request create-method: -Added "Create" in Controller -Aded modal popup to create product in Index.cshtml

--- Eshop/Controllers/ProductController.cs
+++ Eshop/Controllers/ProductController.cs
@@ -21,7 +21,30 @@ namespace Eshop.Controllers
             var products = await _context.Products.
                 Include(p => p.ProductCategory).
                 ToListAsync(cancellationToken);
+            
+            var categories = await _context.ProductCategories.ToListAsync(cancellationToken);
+            ViewBag.Categories = categories;
+
             return View(products);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.ToDictionary(x => x.Key, x => x.Value.Errors.Select(e => e.ErrorMessage).ToArray());
+                return BadRequest(errors);
+            }
+
+            await _context.Products.AddAsync(product, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            // Формируем URL для перенаправления
+            var redirectUrl = Url.Action("Details", new { id = product.Id });
+
+            // Возвращаем JSON-ответ с URL для перенаправления
+            return Json(new { redirectUrl });
         }
         
         [HttpGet]
@@ -62,6 +85,5 @@ namespace Eshop.Controllers
             // Возвращаем JSON-ответ с URL для перенаправления
             return Json(new { redirectUrl });
         }
-
     }
 }
