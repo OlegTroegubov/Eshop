75de5b8efb456ddbc65f4ad1321fd3c26448285e
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-06-30 03:24:25 +0700

    Edited js script

--- Eshop/wwwroot/js/product.js
+++ Eshop/wwwroot/js/product.js
@@ -5,7 +5,7 @@
     }
 
     // Do not display the item if there is no 'text' property
-    if (typeof data.text === 'undefined') {
+    if (typeof data.text === 'undefined') {  
         return null;
     }
 
@@ -13,7 +13,6 @@
     // `data.text` is the text that is displayed for the data object
     if (data.text.indexOf(params.term) > -1) {
         var modifiedData = $.extend({}, data, true);
-        modifiedData.text += ' (matched)';
 
         // You can return modified objects from here
         // This includes matching the `children` how you want in nested data sets
