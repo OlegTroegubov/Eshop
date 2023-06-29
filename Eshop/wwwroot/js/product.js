4aaa2e8e295abd44e81b33375f1d3b8642094341
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-06-30 02:30:55 +0700

    Created js script: New dropdown list for categories now we can find a category

---
+++ Eshop/wwwroot/js/product.js
@@ -0,0 +1,37 @@
+﻿function Find(params, data) {
+    // If there are no search terms, return all of the data
+    if ($.trim(params.term) === '') {
+        return data;
+    }
+
+    // Do not display the item if there is no 'text' property
+    if (typeof data.text === 'undefined') {
+        return null;
+    }
+
+    // `params.term` should be the term that is used for searching
+    // `data.text` is the text that is displayed for the data object
+    if (data.text.indexOf(params.term) > -1) {
+        var modifiedData = $.extend({}, data, true);
+        modifiedData.text += ' (matched)';
+
+        // You can return modified objects from here
+        // This includes matching the `children` how you want in nested data sets
+        return modifiedData;
+    }
+
+    // Return `null` if the term should not be displayed
+    return null;
+}
+
+$("#add-category-list").select2({
+    matcher: Find,
+    dropdownParent: $('#add-modal-content')
+});
+$("#edit-category-list").select2({
+    matcher: Find,
+    dropdownParent: $('#edit-modal-content')
+});
+$('#add-category-list').next('.select2-container').css('display', 'block');
+$('#edit-category-list').next('.select2-container').css('display', 'block');
+$('.select2-selection.select2-selection--single').css('height', '36px');
