753f643cd7af8727564d919110cd55b855b6b741
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-02 05:00:48 +0700

    Added new script: -Takes productCategories from json and put it in select -Edited some code in Views

---
+++ Eshop/wwwroot/js/productCategories.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+    $.ajax({
+        url: '/Product/GetCategories',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            const categories = data;
+            const select = $('#add-category-list');
+            $.each(categories, function(index, {id, name}) {
+                select.append($('<option></option>').val(id).text(name));
+            });
+        },
+    });
+});
