286c02b6ffae77ceec1777c76ab8c8bcd8e98beb
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-02 04:08:18 +0700

    Created ProductCategoryService

---
+++ Eshop/Services/ProductCategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Eshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshop.Services;
+
+public class ProductCategoryService
+{
+    private readonly ApplicationDbContext _context;
+    public ProductCategoryService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<ProductCategory>> GetCategoriesAsync()
+    {
+        return await _context.ProductCategories.ToListAsync();
+    }
+}
