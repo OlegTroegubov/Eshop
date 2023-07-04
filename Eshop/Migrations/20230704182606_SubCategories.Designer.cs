be5aa5380c5824faa003b7377dbdb92daf77d804
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-05 03:40:20 +0700

    New Migration

--- Eshop/Migrations/20230704182606_SubCategories.Designer.cs
+++ Eshop/Migrations/20230704182606_SubCategories.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Eshop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Eshop.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230704182606_SubCategories")]
+    partial class SubCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
