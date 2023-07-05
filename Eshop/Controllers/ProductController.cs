ef9d6c864b7fcc7265400537fe607d419dd9a8a5
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-05 08:43:02 +0700

    Edited Controller

--- Eshop/Controllers/ProductController.cs
+++ Eshop/Controllers/ProductController.cs
@@ -9,23 +9,19 @@ namespace Eshop.Controllers
 {
     public class ProductController : Controller
     {
-        private readonly ApplicationDbContext _context;
         private readonly ProductCategoryService _categoryService;
-        public ProductController(ApplicationDbContext context, ProductCategoryService categoryService)
+        private readonly ProductService _productService;
+        public ProductController(ProductCategoryService categoryService, ProductService productService)
         {
-            _context = context;
             _categoryService = categoryService;
+            _productService = productService;
         }
 
         // GET: Product
         [HttpGet]
         public async Task<IActionResult> Index(CancellationToken cancellationToken)
         {
-            var products = await _context.Products.
-                Include(p => p.SubProductCategory).
-                ToListAsync(cancellationToken);
-
-            return View(products);
+            return View(await _productService.GetProductsAsync(cancellationToken));
         }
         
         [HttpPost]
@@ -33,36 +29,27 @@ namespace Eshop.Controllers
         {
             if (!ModelState.IsValid)
             {
-                var errors = ModelState.ToDictionary(x => x.Key, x => x.Value.Errors.Select(e => e.ErrorMessage).ToArray());
+                var errors = ModelState
+                    .ToDictionary(x => x.Key, x => x.Value.Errors
+                    .Select(e => e.ErrorMessage)
+                    .ToArray());
                 return BadRequest(errors);
             }
 
-            await _context.Products.AddAsync(product, cancellationToken);
-            await _context.SaveChangesAsync(cancellationToken);
-
-            // Формируем URL для перенаправления
-            var redirectUrl = Url.Action("Details", new { id = product.Id });
-
-            // Возвращаем JSON-ответ с URL для перенаправления
-            return Json(new { redirectUrl });
+            await _productService.AddAsync(product, cancellationToken);
+            return Json(new { redirectUrl= Url.Action("Details", new { id = product.Id})});
         }
         
         [HttpGet]
         public async Task<IActionResult> Details(int id, CancellationToken cancellationToken)
         {
-            var product = await _context.Products.
-                Include(p => p.SubProductCategory).
-                FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
-            
-            return View(product);
+            return View(await _productService.GetProductByIdAsync(id, cancellationToken));
         }
 
         [HttpPost]
         public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
         {
-            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
-            _context.Products.Remove(product);
-            await _context.SaveChangesAsync(cancellationToken);
+            await _productService.DeleteAsync(id, cancellationToken);
             return RedirectToAction("Index");
         }
         
@@ -71,24 +58,21 @@ namespace Eshop.Controllers
         {
             if (!ModelState.IsValid)
             {
-                var errors = ModelState.ToDictionary(x => x.Key, x => x.Value.Errors.Select(e => e.ErrorMessage).ToArray());
+                var errors = ModelState
+                    .ToDictionary(x => x.Key, x => x.Value.Errors
+                    .Select(e => e.ErrorMessage)
+                    .ToArray());
                 return BadRequest(errors);
             }
 
-            _context.Products.Update(product);
-            await _context.SaveChangesAsync(cancellationToken);
-
-            // Формируем URL для перенаправления
-            var redirectUrl = Url.Action("Details", new { id = product.Id });
-
-            // Возвращаем JSON-ответ с URL для перенаправления
-            return Json(new { redirectUrl });
+            await _productService.EditAsync(product, cancellationToken);
+            return Json(new { redirectUrl= Url.Action("Details", new { id = product.Id})});
         }
         
         [HttpGet]
-        public async Task<IActionResult> GetCategories()
+        public async Task<IActionResult> GetCategories(CancellationToken cancellationToken)
         {
-            return Json(await _categoryService.GetCategoriesAsync());
+            return Json(await _categoryService.GetCategoriesAsync(cancellationToken));
         }
     }
 }
