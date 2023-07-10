64d3dc39d7524312d817c218cd231cde64bf70db
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-10 10:32:11 +0700

    Edited DbSeeder

--- Eshop/Persistence/Configuration/DbSeeder.cs
+++ Eshop/Persistence/Configuration/DbSeeder.cs
@@ -21,7 +21,7 @@ public class DbSeeder
             return;
         }
 
-        var parentCategories = Enumerable.Range(1, 100)
+        var parentCategories = Enumerable.Range(1, 3)
             .Select(x => new ProductCategory
             {
                 Name = $"Категория №{x}",
@@ -29,20 +29,38 @@ public class DbSeeder
             });
         await _context.AddRangeAsync(parentCategories);
         await _context.SaveChangesAsync();
-        var categories = Enumerable.Range(101, 200)
-            .Select(x => new ProductCategory
-            {
-                Name = $"Категория №{x}",
-                ParentProductCategoryId = x - 100
-            }); 
+        var categories = Enumerable.Range(1, 3)
+            .SelectMany(parentIndex =>
+                Enumerable.Range(1, 5)
+                    .Select(childIndex => new ProductCategory
+                    {
+                        Name = $"Категория №{parentIndex}.{childIndex}",
+                        ParentProductCategoryId = parentIndex
+                    })
+            )
+            .ToList();
         await _context.AddRangeAsync(categories);
         await _context.SaveChangesAsync();
-        
-        var products = Enumerable.Range(1, 100)
+        var subCategories = Enumerable.Range(1, 3)
+            .SelectMany(parentIndex =>
+                Enumerable.Range(1, 5)
+                    .SelectMany(childIndex =>
+                        Enumerable.Range(1, 10)
+                            .Select(nestedIndex => new ProductCategory
+                            {
+                                Name = $"Ккатегория №{parentIndex}.{childIndex}.{nestedIndex}",
+                                ParentProductCategoryId = categories[(parentIndex - 1) * 5 + (childIndex - 1)].Id
+                            })
+                    )
+            )
+            .ToList();
+        await _context.AddRangeAsync(subCategories);
+        await _context.SaveChangesAsync();
+        var products = Enumerable.Range(1, 160)
             .Select(x => new Product
             {
                 ///Добавляем только последние вложенные по иерархии категории товара
-                ProductCategoryId = new Random().Next(200, 300),
+                ProductCategoryId = new Random().Next(19, 168),
                 Title = $"Продукт №{x}",
                 Price = new Random().Next(100, 3000)
             }).
