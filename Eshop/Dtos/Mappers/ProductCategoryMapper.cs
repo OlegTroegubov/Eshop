53033af7f510089dd69fb858d9ed83a5b38a44ea
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-17 04:15:09 +0700

    Added mappers: -ProductMapper -ProductCategoryMapper

---
+++ Eshop/Dtos/Mappers/ProductCategoryMapper.cs
@@ -0,0 +1,34 @@
+﻿namespace Eshop.Dtos.Mappers;
+
+using Eshop.Models;
+using Eshop.Dtos.ProductCategory;
+
+public static class ProductCategoryMapper
+{
+    public static ProductCategoryDto MapToDto(ProductCategory productCategory)
+    {
+        if (productCategory == null)
+            return null;
+
+        return new ProductCategoryDto
+        {
+            Id = productCategory.Id,
+            Name = productCategory.Name,
+            ParentProductCategory = MapToDto(productCategory.ParentProductCategory)
+        };
+    }
+
+    public static ProductCategory MapToProductCategory(ProductCategoryDto productCategoryDto)
+    {
+        if (productCategoryDto == null)
+            return null;
+
+        return new ProductCategory
+        {
+            Id = productCategoryDto.Id,
+            Name = productCategoryDto.Name,
+            ParentProductCategoryId = productCategoryDto.ParentProductCategory?.Id,
+            ParentProductCategory = MapToProductCategory(productCategoryDto.ParentProductCategory)
+        };
+    }
+}
---
+++ Eshop/Dtos/Mappers/ProductMapper.cs
@@ -0,0 +1,36 @@
+﻿using Eshop.Dtos.Product;
+
+namespace Eshop.Dtos.Mappers;
+
+public static class ProductMapper
+{
+    public static ProductDto MapToDto(Models.Product product)
+    {
+        if (product == null)
+            return null;
+
+        return new ProductDto
+        {
+            Id = product.Id,
+            ProductCategoryId = product.ProductCategoryId,
+            Title = product.Title,
+            Price = product.Price,
+            ProductCategory = ProductCategoryMapper.MapToDto(product.ProductCategory)
+        };
+    }
+
+    public static Models.Product MapToProduct(ProductDto productDto)
+    {
+        if (productDto == null)
+            return null;
+
+        return new Models.Product
+        {
+            Id = productDto.Id,
+            ProductCategoryId = productDto.ProductCategoryId,
+            Title = productDto.Title,
+            Price = productDto.Price,
+            ProductCategory = ProductCategoryMapper.MapToProductCategory(productDto.ProductCategory)
+        };
+    }
+}
