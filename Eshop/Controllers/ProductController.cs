1cf428ba0947270a3f5490866321174043f57222
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-17 04:16:44 +0700

    Edited logic for Dto: -In Controllers -In Views

--- Eshop/Controllers/ProductController.cs
+++ Eshop/Controllers/ProductController.cs
@@ -1,74 +1,67 @@
-﻿using Eshop.Models;
+﻿using Eshop.Dtos.Product;
 using Eshop.Services;
 using Microsoft.AspNetCore.Mvc;
 
+namespace Eshop.Controllers;
 
-namespace Eshop.Controllers
+public class ProductController : Controller
 {
-    public class ProductController : Controller
+    private readonly ProductService _productService;
+
+    public ProductController(ProductService productService)
     {
-        private readonly ProductService _productService;
-        public ProductController(ProductService productService)
-        {
-            _productService = productService;
-        }
+        _productService = productService;
+    }
 
-        // GET: Product
-        [HttpGet]
-        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    // GET: Product
+    [HttpGet]
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        return View(await _productService.GetProductsAsync(cancellationToken));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ProductDto product, CancellationToken cancellationToken)
+    {
+        if (!ModelState.IsValid)
         {
-            return View(await _productService.GetProductsAsync(cancellationToken));
-        }
-        
-        [HttpPost]
-        public async Task<IActionResult> Create(Product product, CancellationToken cancellationToken)
-        {
-            if (!ModelState.IsValid)
-            {
-                var errors = ModelState
-                    .ToDictionary(x => x.Key, x => x.Value.Errors
+            var errors = ModelState
+                .ToDictionary(x => x.Key, x => x.Value.Errors
                     .Select(e => e.ErrorMessage)
                     .ToArray());
-                return BadRequest(errors);
-            }
-
-            await _productService.AddAsync(product, cancellationToken);
-            return Json(new { redirectUrl= Url.Action("Details", new { id = product.Id})});
-        }
-        
-        [HttpGet]
-        public async Task<IActionResult> Details(int id, CancellationToken cancellationToken)
-        {
-            return View(await _productService.GetProductByIdAsync(id, cancellationToken));
+            return BadRequest(errors);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+        var createdProduct = await _productService.AddAsync(product, cancellationToken);
+        return Json(new { redirectUrl = Url.Action("Details", new { id = createdProduct.Id }) });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int id, CancellationToken cancellationToken)
+    {
+        return View(await _productService.GetProductByIdAsync(id, cancellationToken));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+    {
+        await _productService.DeleteAsync(id, cancellationToken);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ProductDto product, CancellationToken cancellationToken)
+    {
+        if (!ModelState.IsValid)
         {
-            await _productService.DeleteAsync(id, cancellationToken);
-            return RedirectToAction("Index");
-        }
-        
-        [HttpPost]
-        public async Task<IActionResult> Edit(Product product, CancellationToken cancellationToken)
-        {
-            if (!ModelState.IsValid)
-            {
-                var errors = ModelState
-                    .ToDictionary(x => x.Key, x => x.Value.Errors
+            var errors = ModelState
+                .ToDictionary(x => x.Key, x => x.Value.Errors
                     .Select(e => e.ErrorMessage)
                     .ToArray());
-                return BadRequest(errors);
-            }
+            return BadRequest(errors);
+        }
 
-            await _productService.EditAsync(product, cancellationToken);
-            return Json(new { redirectUrl= Url.Action("Details", new { id = product.Id})});
-        }
-        
-        [HttpGet]
-        public async Task<IActionResult> GetProductsByCategory(int id,CancellationToken cancellationToken)
-        {
-            return View("Index", await _productService.GetProductsByCategoryAsync(id, cancellationToken));
-        }
+        await _productService.EditAsync(product, cancellationToken);
+        return Json(new { redirectUrl = Url.Action("Details", new { id = product.Id }) });
     }
 }
