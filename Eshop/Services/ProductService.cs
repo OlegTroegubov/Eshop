9fa0fb31fb27a56744bdb6888f3be49b422bb3de
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-17 04:16:03 +0700

    Edited logic for Dto

--- Eshop/Services/ProductService.cs
+++ Eshop/Services/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using Eshop.Models;
+﻿using Eshop.Dtos.Mappers;
+using Eshop.Dtos.Product;
+using Eshop.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Eshop.Services;
@@ -11,95 +13,40 @@ public class ProductService
     {
         _context = context;
     }
-    
-    public async Task<List<Product>> GetProductsAsync(CancellationToken cancellationToken)
+
+    public async Task<List<ProductDto>> GetProductsAsync(CancellationToken cancellationToken)
     {
         return await _context.Products
             .Include(product => product.ProductCategory)
+            .Select(x => ProductMapper.MapToDto(x))
             .ToListAsync(cancellationToken);
     }
 
-    public async Task<List<Product>> GetProductsByCategoryAsync(int id, CancellationToken cancellationToken)
+    public async Task<ProductDto> GetProductByIdAsync(int productId, CancellationToken cancellationToken)
     {
-        var products = await GetProductsAsync(cancellationToken);
-        await IncludeCategories(products);
-        
-        var sortedProducts = new List<Product>();
-        foreach (var product in products)
-        {
-            if (product.ProductCategoryId == id)
-            {
-                sortedProducts.Add(product);
-            }
-            else
-            {
-                if (IsContainsCategory(product.ProductCategory, id))
-                {
-                    sortedProducts.Add(product);
-                }
-            }
-        }
-        return sortedProducts;
-    }
-
-    
-    public async Task<Product> GetProductByIdAsync(int id, CancellationToken cancellationToken)
-    {
-        return await _context.Products
+        return ProductMapper.MapToDto(await _context.Products
             .Include(product => product.ProductCategory)
-            .FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+            .FirstOrDefaultAsync(p => p.Id == productId, cancellationToken));
     }
-    
-    public async Task AddAsync(Product product, CancellationToken cancellationToken)
+
+    public async Task<ProductDto> AddAsync(ProductDto product, CancellationToken cancellationToken)
     {
-        await _context.Products.AddAsync(product, cancellationToken);
+        var productToAdd = ProductMapper.MapToProduct(product);
+        await _context.Products.AddAsync(productToAdd, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return ProductMapper.MapToDto(productToAdd);
+    }
+
+    public async Task EditAsync(ProductDto product, CancellationToken cancellationToken)
+    {
+        _context.Products.Update(ProductMapper.MapToProduct(product));
         await _context.SaveChangesAsync(cancellationToken);
     }
 
-    public async Task EditAsync(Product product, CancellationToken cancellationToken)
+    public async Task DeleteAsync(int productId, CancellationToken cancellationToken)
     {
-        _context.Products.Update(product);
+        _context.Products.Remove(
+            await _context.Products.FirstOrDefaultAsync(p => p.Id == productId, cancellationToken));
         await _context.SaveChangesAsync(cancellationToken);
-    }
-    
-    public async Task DeleteAsync(int id, CancellationToken cancellationToken)
-    {
-        _context.Products.Remove(await GetProductByIdAsync(id, cancellationToken));
-        await _context.SaveChangesAsync(cancellationToken);
-    }
-
-    private bool IsContainsCategory(ProductCategory category, int id)
-    {
-        if (category == null)
-        {
-            return false;
-        }
-        
-        if (category.Id == id)
-        {
-            return true;
-        }
-
-        return IsContainsCategory(category.ParentProductCategory, id);
-    }
-    
-    private async Task IncludeCategories(List<Product> products)
-    {
-        foreach (var product in products)
-        {
-            await IncludeCategories(product.ProductCategory);
-        }
-    }
-
-    private async Task IncludeCategories(ProductCategory category)
-    {
-        if (category != null)
-        {
-            await _context.Entry(category)
-                .Reference(c => c.ParentProductCategory)
-                .LoadAsync();
-
-            await IncludeCategories(category.ParentProductCategory);
-        }
     }
 }
