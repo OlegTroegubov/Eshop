f9ed8728502bc6baf52bf8dc532f2f4c79d28066
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-20 09:43:04 +0700

    Added CategoryHierarchyDto

---
+++ Eshop/Dtos/ProductCategory/ProductCategoryHierarchyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Eshop.Dtos.ProductCategory;
+
+public class ProductCategoryHierarchyDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<ProductCategoryHierarchyDto> ChildrenCategories { get; set; }
+}
