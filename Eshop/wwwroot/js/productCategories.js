f2a1fcc5b5e812d814e61f0f3a4a0dbba9ed90b3
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-23 07:59:25 +0700

    Added new dropdownlist for filter products

--- Eshop/wwwroot/js/productCategories.js
+++ Eshop/wwwroot/js/productCategories.js
@@ -6,20 +6,23 @@
         success: function (data) {
             const categories = data;
             const select = $('#add-category-list, #edit-category-list');
+            const select1 = $('#select-category-list');
 
             function appendOptions(categories, indentation = "") {
                 $.each(categories, function (index, category) {
                     const hasChildren = category.childrenCategories.length > 0;
 
                     if (hasChildren) {
-                        const optgroup = $('<option></option>').html(indentation + category.name).val(category.id)
-                            .prop('disabled', true);
+                        const optgroup = $('<option></option>').html(indentation + category.name).val(category.id).prop('disabled', true);
                         select.append(optgroup);
 
+                        const option = $('<option></option>').html(indentation + category.name).val(category.id);
+                        select1.append(option);
                         appendOptions(category.childrenCategories, indentation + "&nbsp;&nbsp;&nbsp;&nbsp;");
                     } else {
                         const option = $('<option></option>').html(indentation + category.name).val(category.id);
                         select.append(option);
+                        select1.append($('<option></option>').html(indentation + category.name).val(category.id))
                     }
                 });
             }
