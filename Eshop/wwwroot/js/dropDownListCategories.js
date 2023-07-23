aedda3a800f1c883d566ca74cef2fae03027bf4a
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-23 08:28:03 +0700

    Created ajax-request which gets filtred products

--- Eshop/wwwroot/js/dropDownListCategories.js
+++ Eshop/wwwroot/js/dropDownListCategories.js
@@ -38,6 +38,29 @@ $("#edit-category-list").select2({
     $('#select2-edit-category-list-container').text(trimmedValue);
 });
 
+// Отдельный метод для обработки выбора категории
+function handleCategorySelect(e) {
+    const selectedValue = e.params.data.text;
+    const trimmedValue = selectedValue.trim();
+    $('#select2-select-category-list-container').text(trimmedValue);
+    var selectedItem = e.params.data.id;
+    var url = '/Product/GetProductsByCategory/' + selectedItem;
+    $.get(url).then(function (data) {
+        $('#table').data('table-products', data);
+        $('#table').bootstrapTable('load', data);
+    });
+    var table = $('#table');
+    table.attr('data-filtred', 'true');
+}
+
+// Инициализация select2 и навешивание обработчика
+$("#select-category-list").select2({
+    placeholder: "Все категории",
+    matcher: Find,
+}).on('select2:select', handleCategorySelect);
+
+
 $('#add-category-list').next('.select2-container').css('display', 'block');
 $('#edit-category-list').next('.select2-container').css('display', 'block');
+$('#select-category-list').next('.select2-container').css('display', 'block');
 $('.select2-selection.select2-selection--single').css('height', '36px');
