97fa8f0c3f3f199e8e7dd00cd03c8b3fe2ee94a6
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-29 18:21:00 +0700

    Fixed

--- Eshop/wwwroot/js/productCategories.js
+++ Eshop/wwwroot/js/productCategories.js
@@ -6,12 +6,7 @@
         success: function (data) {
             const categories = data;
             const select = $('#add-category-list, #edit-category-list');
-<<<<<<< HEAD
-            const select1 = $('#select-category-list');
-
-=======
             const searchSelect = $('#select-category-list');
->>>>>>> feature
             function appendOptions(categories, indentation = "") {
                 $.each(categories, function (index, category) {
                     const hasChildren = category.childrenCategories.length > 0;
@@ -21,24 +16,14 @@
                         select.append(optgroup);
 
                         const option = $('<option></option>').html(indentation + category.name).val(category.id);
-<<<<<<< HEAD
-                        select1.append(option);
-=======
                         searchSelect.append(option);
->>>>>>> feature
                         appendOptions(category.childrenCategories, indentation + "&nbsp;&nbsp;&nbsp;&nbsp;");
                     } else {
                         const option = $('<option></option>').html(indentation + category.name).val(category.id);
                         select.append(option);
-<<<<<<< HEAD
-                        select1.append($('<option></option>').html(indentation + category.name).val(category.id))
-=======
-                        searchSelect.append($('<option></option>').html(indentation + category.name).val(category.id))
->>>>>>> feature
                     }
                 });
             }
-
             appendOptions(categories);
         },
     });
