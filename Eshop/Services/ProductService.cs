59aba7ed24f3b595f56440671a1ea4c9b8f9a2a0
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-07-30 21:37:07 +0700

    Fixed filtring logic

--- Eshop/Services/ProductService.cs
+++ Eshop/Services/ProductService.cs
@@ -25,35 +25,46 @@ public class ProductService
     public async Task<List<ProductDto>> GetProductsAsync(int categoryId, string sortName,
         string sortOrder, CancellationToken cancellationToken)
     {
-        var products = await _context.Products.Include(product => product.ProductCategory).ToListAsync(cancellationToken);
-
+        var products = _context.Products.AsQueryable();
         if (categoryId != 0)
         {
-            products = products.Where(product => IsContainsCategory(product.ProductCategory, categoryId).Result).ToList();
-        }
+            var category = await _context.ProductCategories.AsNoTracking()
+                .Include(productCategory => productCategory.ChildrenProductCategory)
+                    .ThenInclude(productCategory => productCategory.ChildrenProductCategory)
+                .FirstOrDefaultAsync(productCategory => productCategory.Id == categoryId, cancellationToken);
 
+            var categoryIds = category.ChildrenProductCategory
+                .SelectMany(childCategory => childCategory.ChildrenProductCategory
+                    .Select(grandchildCategory => grandchildCategory.Id)
+                    .Union(new List<int> { childCategory.Id })) 
+                .ToList();
+            categoryIds.Add(categoryId);
+            
+            products = products.Where(product => categoryIds.Contains(product.ProductCategoryId));
+        }
         if (!string.IsNullOrEmpty(sortName) && !string.IsNullOrEmpty(sortOrder))
         {
             products = sortName switch
             {
                 "price" => sortOrder switch
                 {
-                    "asc" => products.OrderBy(product => product.Price).ToList(),
-                    "desc" => products.OrderByDescending(product => product.Price).ToList(),
+                    "asc" => products.OrderBy(product => product.Price),
+                    "desc" => products.OrderByDescending(product => product.Price),
                     _ => products
                 },
                 "title" => sortOrder switch
                 {
-                    "asc" => products.OrderBy(product => product.Title).ToList(),
-                    "desc" => products.OrderByDescending(product => product.Title).ToList(),
+                    "asc" => products.OrderBy(product => product.Title),
+                    "desc" => products.OrderByDescending(product => product.Title),
                     _ => products
                 },
                 _ => products
             };
         }
 
-        return products
-            .Select(product => ProductMapper.MapToDto(product)).ToList();
+        return await products
+            .Include(product => product.ProductCategory)
+            .Select(product => ProductMapper.MapToDto(product)).ToListAsync(cancellationToken);
     }
 
     /// <summary>
@@ -106,41 +117,4 @@ public class ProductService
             await _context.Products.FirstOrDefaultAsync(p => p.Id == productId, cancellationToken));
         await _context.SaveChangesAsync(cancellationToken);
     }
-
-    #region privateMethods
-
-    /// <summary>
-    ///     Проверяет, содержит ли категория категорию, которую мы ищем.
-    /// </summary>
-    /// <param name="productCategory">Категория продукта.</param>
-    /// <param name="categoryId">Id категории, которую мы ищем.</param>
-    /// <returns>Результат false - продукт не относится к категории.</returns>
-    /// <returns>Результат true - продукт относится к категории</returns>
-    private async Task<bool> IsContainsCategory(ProductCategory productCategory, int categoryId)
-    {
-        if (productCategory == null) return false;
-        await IncludeCategories(productCategory);
-        if (productCategory.Id == categoryId) return true;
-
-        return await IsContainsCategory(productCategory.ParentProductCategory, categoryId);
-    }
-
-    /// <summary>
-    ///     Загружает ссылки на родительские категории, если такие имеются.
-    /// </summary>
-    /// <param name="category">Категория продукта.</param>
-    /// <returns>Категорию со всеми вложенными родительскими категорями</returns>
-    private async Task IncludeCategories(ProductCategory category)
-    {
-        if (category != null)
-        {
-            await _context.Entry(category)
-                .Reference(c => c.ParentProductCategory)
-                .LoadAsync();
-
-            await IncludeCategories(category.ParentProductCategory);
-        }
-    }
-
-    #endregion
 }
