729772ee9e6baaacc115dfa29317c19b48ba962d
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-08-23 02:35:15 +0700

    Created new exception

---
+++ Eshop.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Eshop.Application.Common.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+    {
+    }
+
+    public NotFoundException(string message) : base(message)
+    {
+    }
+}
