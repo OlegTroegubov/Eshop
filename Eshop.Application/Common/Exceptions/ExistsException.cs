3c33d77f9a4ebf22bd668c6edc0ad00ce4b42d7f
OlegTroegubov <troegubov.oleg.kem@gmail.com>
2023-08-29 15:16:55 +0700

    Added new exception

---
+++ Eshop.Application/Common/Exceptions/ExistsException.cs
@@ -0,0 +1,14 @@
+﻿namespace Eshop.Application.Common.Exceptions;
+
+public class ExistsException : Exception
+{
+    public ExistsException()
+    {
+    }
+
+    public ExistsException(string message) : base(message)
+    {
+
+    }
+}
+
